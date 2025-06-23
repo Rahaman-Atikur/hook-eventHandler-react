a49bd490c89144fb3cedcb4d62baf5cba01edd42
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-23 21:20:23 +0100

    Clicked handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import './App.css'
 function App() {
+  function handleClick(){
+    alert("I am Clicked");
+  }
+  const anotherButton=()=>{
+    alert("I am also Clicked");
+  }
+  const addNumber=(num)=>{
+    let number = num + 5;
+    alert(number);
+  }
     return (
     <>
       <h1>React</h1>
+      <button onClick={handleClick}>Click Me</button> <br />
+      <button onClick={anotherButton}>Click Me</button> <br />
+      <button onClick={()=>addNumber(10)}>ADD NUMBER</button>
+    
     </>
   )
 }
