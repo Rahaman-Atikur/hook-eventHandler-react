c39bb1bc08ec953f11ae5d14a4ee9bd3a1cb6c92
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-24 21:29:40 +0100

    UseState clear

--- src/batsman.jsx
+++ src/batsman.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 export default function BatsMan() {
     const [runs, setRuns] = useState(0);
+    const [sixes,setSixes] = useState(0);
     const handleSingle = () => {
         let updatedRuns = runs + 1;
         setRuns(updatedRuns);
     }
     const handleSix = () => {
         let updatedRuns = runs + 6;
+        const updatedSixes = sixes + 1;
+        setSixes(updatedSixes);
         setRuns(updatedRuns);
     }
     const handleFour = () => {
@@ -17,6 +20,7 @@ export default function BatsMan() {
         <div>
             <h3>Player: Bangla BatsMan</h3>
             <p>Score:{runs} </p>
+            <p>Sixes:{sixes}</p>
             <button onClick={handleSingle}>Singles</button>
             <button onClick={handleFour}>Four</button>
             <button onClick={handleSix}>Six</button>
