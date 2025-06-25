a8b69435cd182194015aa8dc2c6750712c041320
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-25 18:59:51 +0100

    Task Two Completely Done

--- src/showText.jsx
+++ src/showText.jsx
@@ -1,18 +1,20 @@
+import { use, useState } from "react";
+
 export default function ShowText() {
     const [text,setText] = useState(0);
     const showText=()=>{
-
+        let newText = "Hi, I am learning React";
+        setText(newText);
     }
     const hideText=()=>{
-        text="";
-
+        let newText = "";
+        setText(newText);
     }
     return (
         <div className="reseter">
             <button onClick={showText} className="card">Show</button>
             <button onClick={hideText} className="card">Hide</button>
             <h3>{text}</h3>
-
         </div>
     )
 }
